b4b1bd75ef49a15ad6e1e429d439e3b6de8cdc30
Leo <1093038078@qq.com>
2015-05-04 23:34:31 +0800

    add api to show helloworld

---
+++ app/code/local/Imboutique/Extenstion/controllers/ExtenstionController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Leo
+ * Date: 15/5/4
+ * Time: 下午11:33
+ */
+class Imboutique_Extenstion_ExtenstionController extends Mage_Core_Controller_Front_Action
+{
+    public function indexAction() {
+        echo "Hello World;";
+    }
+    public function showworldAction() {
+        echo "Welcome to Magento World;";
+    }
+}
