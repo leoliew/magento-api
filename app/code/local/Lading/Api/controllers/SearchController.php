6da635a6b8e0658c0468c964489c2274b73e8822
Leo <zliu@kalengo.com>
2015-07-09 18:59:14 +0800

    search api

--- app/code/local/Lading/Api/controllers/SearchController.php
+++ app/code/local/Lading/Api/controllers/SearchController.php
@@ -42,14 +42,12 @@ class Lading_Api_SearchController extends Mage_Core_Controller_Front_Action {
 			}
 			Mage::helper('catalogsearch')->checkNotes();
 			$collection = $query->getResultCollection();
-			$collection = $collection->addAttributeToFilter('visibility', array('in' => array(
+			$collection->setCurPage($page)->setPageSize($limit)->addAttributeToFilter('visibility', array('in' => array(
 				Mage_Catalog_Model_Product_Visibility::VISIBILITY_IN_SEARCH,
 				Mage_Catalog_Model_Product_Visibility::VISIBILITY_BOTH,
 			)))->addAttributeToSort ( $order, $dir );
-			$result_count = count($collection);
-			$pages = $collection->setPageSize ( $limit )->getLastPageNumber ();
+			$pages = $collection->setPageSize($limit)->getLastPageNumber();
 			if($page <= $pages){
-				$collection->setCurPage($page)->setPageSize($limit);
 				$i = 1;
 				$baseCurrency = Mage::app()->getStore()->getBaseCurrency()->getCode();
 				$currentCurrency = Mage::app()->getStore()->getCurrentCurrencyCode();
@@ -88,7 +86,6 @@ class Lading_Api_SearchController extends Mage_Core_Controller_Front_Action {
 					'msg'=>'search '.count($collection).' product success!',
 					'model'=> array(
 						'items'=> $product_list,
-						'count' => $result_count
 					)
 				)
 			);
@@ -107,72 +104,59 @@ class Lading_Api_SearchController extends Mage_Core_Controller_Front_Action {
 		}
 	}
 
-	public function index2Action(){
-		$searchText = 'oxford';
-		$query = Mage::getModel('catalogsearch/query')->setQueryText($searchText)->prepare();
-		$fulltextResource = Mage::getResourceModel('catalogsearch/fulltext')->prepareResult(
-			Mage::getModel('catalogsearch/fulltext'),
-			$searchText,
-			$query
-		);
-
-		$collection = Mage::getResourceModel('catalog/product_collection');
-		$collection->getSelect()->joinInner(
-			array('search_result' => $collection->getTable('catalogsearch/result')),
-			$collection->getConnection()->quoteInto(
-				'search_result.product_id=e.entity_id AND search_result.query_id=?',
-				$query->getId()
-			),
-			array('relevance' => 'relevance')
-		);
 
 
-		$collection = $query->getResultCollection();
-		$result_count = count($collection);
-
-			$i = 1;
-			$baseCurrency = Mage::app()->getStore()->getBaseCurrency()->getCode();
-			$currentCurrency = Mage::app()->getStore()->getCurrentCurrencyCode();
-			$store_id = Mage::app()->getStore()->getId();
-			foreach($collection as $product){
-				$product = Mage::getModel('catalog/product')->load($product->getId());
-				$summaryData = Mage::getModel('review/review_summary')->setStoreId($store_id) ->load($product->getId());
-				$price =($product->getSpecialPrice()) == null ? ($product->getPrice()) : ($product->getSpecialPrice());
-				$regular_price_with_tax = $product->getPrice();
-				$final_price_with_tax = $product->getSpecialPrice();
-				$product_list [] = array(
-					'entity_id' => $product->getId(),
-					'sku' => $product->getSku(),
-					'name' => $product->getName(),
-					'rating_summary' => $summaryData->getRatingSummary(),
-					'reviews_count' => $summaryData->getReviewsCount(),
-					'news_from_date' => $product->getNewsFromDate (),
-					'news_to_date' => $product->getNewsToDate(),
-					'special_from_date' => $product->getSpecialFromDate(),
-					'special_to_date' => $product->getSpecialToDate(),
-					'image_url' => $product->getImageUrl(),
-					'url_key' => $product->getProductUrl(),
-					'price' =>  number_format(Mage::helper('directory')->currencyConvert($price, $baseCurrency, $currentCurrency), 2, '.', '' ),
-					'regular_price_with_tax' => number_format(Mage::helper('directory')->currencyConvert($regular_price_with_tax, $baseCurrency, $currentCurrency), 2, '.', '' ),
-					'final_price_with_tax' => number_format(Mage::helper('directory')->currencyConvert($final_price_with_tax, $baseCurrency, $currentCurrency), 2, '.', '' ),
-					'symbol' => Mage::app()->getLocale()->currency(Mage::app()->getStore()->getCurrentCurrencyCode())->getSymbol()
-				);
-				$i ++;
+	/**
+	 * get search number
+	 */
+	public function getSearchNumAction() {
+		$query = Mage::helper ( 'catalogsearch' )->getQuery();
+		$query->setStoreId ( Mage::app ()->getStore ()->getId () );
+		if ($query->getQueryText () != '') {
+			if (Mage::helper ( 'catalogsearch' )->isMinQueryLength ()){
+				$query->setId( 0 )->setIsActive( 1 )->setIsProcessed( 1 );
+			}else{
+				if ($query->getId ()) {
+					$query->setPopularity ( $query->getPopularity () + 1 );
+				} else {
+					$query->setPopularity ( 1 );
+				}
+				if ($query->getRedirect ()) {
+					$query->save ();
+					$this->getResponse ()->setRedirect ( $query->getRedirect () );
+					return;
+				} else {
+					$query->prepare ();
+				}
 			}
-
-
-		echo json_encode(
-			array(
-				'code'=>0,
-				'msg'=>'search '.count($collection).' product success!',
-				'model'=> array(
-					'items'=> $product_list,
-					'count' => $result_count
+			Mage::helper('catalogsearch')->checkNotes();
+			$collection = $query->getResultCollection();
+			$collection->addAttributeToFilter('visibility', array('in' => array(
+				Mage_Catalog_Model_Product_Visibility::VISIBILITY_IN_SEARCH,
+				Mage_Catalog_Model_Product_Visibility::VISIBILITY_BOTH,
+			)));
+			echo json_encode(
+				array(
+					'code'=>0,
+					'msg'=>'search '.count($collection).' product success!',
+					'model'=> count($collection)
 				)
-			)
-		);
-
+			);
+			if(!Mage::helper('catalogsearch')->isMinQueryLength()){
+				$query->save();
+			}
+		} else {
+			echo json_encode(
+				array(
+					'code'=>0,
+					'msg'=>null,
+					'model'=>null,
+					'error'=>'search keyword can not null!'
+				)
+			);
+		}
 	}
+
 
 }
 
